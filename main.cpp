5d066e2ed7dc12a2f066c6c2c640782a16ee4ba3
Kai DeLorenzo <kaidelorenzo@users.noreply.github.com>
2021-02-27 18:39:51 -0500

    reconfigured into class structure

--- main.cpp
+++ main.cpp
@@ -5,66 +5,69 @@
 
 using namespace std;
 
-namespace vectorStats {
-    double mean(vector<double> numbers);
-    double min(vector<double> numbers, bool preSorted = false);
-    double max(vector<double> numbers, bool preSorted = false);
-    double median(vector<double> numbers, bool preSorted = false);
-};
+class NumberInterpreter{
+    private:
+        vector<double> numbers;
+        bool sorted;
 
-namespace statsGen {
-    vector<double> parseNumbers(string input);
-    void run(vector<double> numbers);
-}
+        double mean();
+        double min();
+        double max();
+        double median();
+        void sortSelf(){ sort(numbers.begin(), numbers.end()); sorted = true; };
+    public:
+        NumberInterpreter(string x);
+        void outputStats();
+};
 
 int main(void){
     cout << "Please enter, on one line, the count of numbers followed by the numbers themselves, separated by spaces." << endl;
     cout << "Enter a blank line to exit.\n" << endl;
 
     string inputLine;
-    vector<double> numbers;
 
     while (getline(cin, inputLine)) {
-        if ( inputLine.length( ) == 0 ) break;
-        numbers = statsGen::parseNumbers(inputLine);
-        statsGen::run(numbers);
+        if ( inputLine.empty()) break;
+        NumberInterpreter numbers(inputLine);
+        numbers.outputStats();
+
     }
 
     return 0;
 }
 
+// This will parse a line of whitespace-separated numbers into an array.
+// The first number is the count of numbers, and the rest are the numbers.
+NumberInterpreter::NumberInterpreter(string input) {
+    istringstream iss(input);
+    int n;
+    iss >> n;
+    vector<double> parsedNumbers(n);
+
+    for (int i = 0; i < n; i++){
+        iss >> parsedNumbers[i];
+    }
+    sorted = false;
+    numbers = parsedNumbers;
+}
+
 // This where the magic happens! Be polite and give the the user the basic stats on their data.
-void statsGen::run(vector<double> numbers){
-    if(numbers.size() == 0){
+void NumberInterpreter::outputStats(){
+    if(numbers.empty()){
         cout << "There are no stats for zero numbers" << endl;
         return;
     }
-    sort(numbers.begin(), numbers.end());
     cout << "Thanks!" << endl;
-    cout << "The minimum is: " << vectorStats::min(numbers, true) << "," << endl;
-    cout << "the maximum is: " << vectorStats::max(numbers, true) << "," << endl;
-    cout << "the median is: " << vectorStats::median(numbers, true) << "," << endl;
-    cout << "and the mean is: " << vectorStats::mean(numbers) << ".\n" << endl;
+    cout << "The minimum is: " << min() << "," << endl;
+    cout << "the maximum is: " << max() << "," << endl;
+    cout << "the median is: " << median() << "," << endl;
+    cout << "and the mean is: " << mean() << ".\n" << endl;
 
     return;
 }
 
-// This will parse a line of whitespace-separated numbers into an array.
-// The first number is the count of numbers, and the rest are the numbers.
-vector<double> statsGen::parseNumbers(string input) {
-    istringstream iss(input);
-    int n;
-    iss >> n;
-    vector<double> numbers(n);
-
-    for (int i = 0; i < n; i++){
-        iss >> numbers[i];
-    }
-    return numbers;
-}
-
 // This function computes the mean of a vector of doubles called "numbers"
-double vectorStats::mean(vector<double> numbers){
+double NumberInterpreter::mean(){
     double sum = 0;
 
     for (unsigned int i = 0; i < numbers.size(); ++i){
@@ -76,24 +79,24 @@ double vectorStats::mean(vector<double> numbers){
 
 // This function computes the minimum value of a vector of doubles called "numbers"
 // If the vector is already sorted from low to high then set preSorted to true to reduce computation
-double vectorStats::min(vector<double> numbers, bool preSorted){
-    if(!preSorted) sort(numbers.begin(), numbers.end());
+double NumberInterpreter::min(){
+    if(!sorted) sortSelf();
 
     return numbers[0];
 }
 
 // This function computes the maximum value of a vector of doubles called "numbers"
 // If the vector is already sorted from low to high then set preSorted to true to reduce computation
-double vectorStats::max(vector<double> numbers, bool preSorted){
-    if(!preSorted) sort(numbers.begin(), numbers.end());
+double NumberInterpreter::max(){
+    if(!sorted) sortSelf();
 
     return numbers[ numbers.size() - 1 ];
 }
 
 // This function computes the median of a vector of doubles called "numbers"
 // If the vector is already sorted from low to high then set preSorted to true to reduce computation
-double vectorStats::median(vector<double> numbers, bool preSorted){
-  if(!preSorted) sort(numbers.begin(), numbers.end());
+double NumberInterpreter::median(){
+  if(!sorted) sortSelf();
 
   // If we have an odd number of elements, the median is the middle one
   if (numbers.size() % 2){
