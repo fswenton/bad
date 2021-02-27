277484b97c64f6c90cab897a0efc341ab5f99c9f
Musab <musabshakeel@gmail.com>
2021-02-27 15:30:12 -0500

    Refactored code: created a wrapper around vector<double>; defined class methods for computing statistics; removed unnecessary comments.

--- main.cpp
+++ main.cpp
@@ -5,120 +5,111 @@
 
 using namespace std;
 
-// This function computes the mean of a vector of doubles called "a"
-double m0(vector<double> a)
-{
-  // This is the sum
-  double s = 0.0;
+/*
+ * Customized version of the standard vector<double> from STL.
+ * Think of this class as a wrapper around vector<double>.
+ *
+ * Defines methods for computing statistics (min, max, median, mean)
+ * for the numerical data contained in vector<double>.
+ */
 
- // Loop through all elements of the array
-  for (unsigned int i = 0; i < a.size(); ++i)
-  {
-    // Add the element to the sum
-    s += a[i];
-  }
+class CustomVector{
+public:
 
-   // The mean is the sum divided by the count---return that
-  return s/a.size();
+    vector<double> numbers;
 
+    CustomVector(std::vector<double> _numbers) {
+        numbers = _numbers;
+    }
+
+    double mean() {
+        double sum = 0.0;
+
+        for (std::vector<double>::iterator numPointer = numbers.begin();
+             numPointer != numbers.end();
+             ++numPointer) {
+            double num = *numPointer;
+            sum += num;
+        }
+
+        return sum/numbers.size();
+    }
+
+    double min() {
+        sort(numbers.begin(), numbers.end());
+        return numbers[0];
+    }
+
+    double max() {
+        sort(numbers.begin(), numbers.end());
+        return numbers[numbers.size() - 1];
+    }
+
+    double median() {
+        sort(numbers.begin(), numbers.end());
+
+        // Array has an even number of elements
+        if (numbers.size() % 2) {
+            return numbers[(numbers.size() - 1) / 2];
+        } else { // Array has an odd number of elements
+            return 0.5 * (numbers[numbers.size() / 2] + numbers[numbers.size() / 2 - 1]);
+        }
+    }
+};
+
+
+/*
+ * Parse a string of whitespace-separated numbers into a vector.
+ *
+ * Args:
+ *      str: string to be parsed
+ * Returns:
+ *      numbers: Newly created vector comprised of numbers the user has inputted
+ */
+
+vector<double> strToVector(string str) {
+    istringstream strStream(str);
+
+    int vecLen = -1;
+
+    strStream >> vecLen; // First digit in input string is count of elements in input array
+
+    vector<double> numbers(vecLen);
+
+    for (int i = 0; i < vecLen; ++i) {
+        strStream >> numbers[i];
+    }
+
+    return numbers;
 }
 
-// This function computes the minimum value of a vector of doubles called "a"
-double m1(vector<double> a)
-{
 
-  // First, we sort the array
-  sort(a.begin(), a.end());
-
-  // Now we just return the first element, which after sorting is the minimum
-  return a[0];
-
+void computeStatistics(CustomVector numbers) {
+    cout << "Thanks!" << endl << endl;
+    cout << "The minimum is: " << numbers.min() << endl;
+    cout << "The maximum is: " << numbers.max() << endl;
+    cout << "The mean is: " << numbers.mean() << endl;
+    cout << "The median is: " << numbers.median() << endl << endl;
 }
 
-// This function computes the maximum value of a vector of doubles called "a"
-double m2(vector<double> a)
-{
 
-  // First, we sort the array
-  sort(a.begin(), a.end());
+int main(void) {
 
-  // Now we just return the last element, which after sorting is the Maximum
-  return a[ a.size() - 1 ];
+  cout << "Please enter, on one line, the count of numbers followed by the numbers themselves, "
+          "separated by spaces.\nEnter a blank line to exit.\n\n";
 
-}
+  string inputStr = "";
+  getline(cin, inputStr);
 
-// This function computes the median of a vector of doubles called "a"
-double m3(vector<double> a)
-{
+  while (!inputStr.empty()) {
 
-  // First, we sort the array
-  sort(a.begin(), a.end());
+    // Create a CustomVector comprised of numbers the user has inputted
+    CustomVector numbers(strToVector(inputStr));
 
-  // This bit's a little tricky
-  if (a.size() % 2)
-  {
-    // If we have an odd number of elements, the median is the middle one
-    return a[ (a.size() - 1 ) / 2];
-  }
-  else
-  {
-    // Otherwise, we average the values of the two middlemost elements
-    return 0.5*(a[ a.size() / 2 ] + a[ a.size() / 2 - 1]);
-  }
+    // Compute and print statistics related to inputted numbers
+    computeStatistics(numbers);
 
-}
-
-// This will parse a line of whitespace-separated numbers into an array
-// The first number is the count of numbers, and the rest are the numbers
-vector<double> s2a(string s) {
-  istringstream iss(s);
-
-  // "n" is the number of elements in our array, which comes first
-  int n;
-
-  iss >> n;
-
-  vector<double> a(n);
-
-  for (int i = 0;
-       i < n;
-       ++i)
-  {
-    iss >> a[i];
-  }
-
-
-  return a;
-
-}
-
-// This where the magic happens!
-void go(string s)
-{
-
-  // Turn our string of input into an array (above)
-  vector<double> a = s2a(s);
-
-  // Be polite and give the the user the basic stats on their data
-  cout << "Thanks!\n" << "The minimum is " << m1(a) << ",\nthe maximum is " << m2(a) << ",\nthe median is " << m3(a) << ",\nand the mean is " << m0(a) << ".\n\n";
-
-  return;
-
-}
-
-int main(void)
-{
-
-  cout << "Please enter, on one line, the count of numbers followed by the numbers themselves, separated by spaces.\nEnter a blank line to exit.\n\n";
-
-  string s;
-
-  while ( getline( cin , s ) ) {
-
-    if ( s.length( ) == 0 )
-        break;
-
-    go(s);
+    getline(cin, inputStr);
 
   }
 
