4bb1c1b878f1c2341f087e20f82cbd6223cc0e60
zeinhorn@middlebury.edu <>
2021-02-27 18:06:16 -0500

    clean up and condence

--- main.cpp
+++ main.cpp
@@ -7,10 +7,7 @@ using namespace std;
 
 class BasicStats{
 public:
-    string str;
-
     BasicStats(string in_str){
-        // Turn our string of input into an array (above)
         stringToArray(in_str);
     }
 
@@ -25,28 +22,22 @@ public:
     }
 
     double min(){
-      sort(array.begin(), array.end());
       return array[0];
     }
 
     double max(){
-      sort(array.begin(), array.end());
       return array[ array.size() - 1 ];
     }
 
     double median(){
-      sort(array.begin(), array.end());
-
       if (array.size() % 2){
-         //if even
+         //if odd len %2 = 1
         return array[ (array.size() - 1 ) / 2];
       } else {
-        // if odd, we average the values of the two middlemost elements
+        // if even len, we average the values of the two middlemost elements
         return 0.5*(array[ array.size() / 2 ] + array[ array.size() / 2 - 1]);
       }
-
     }
-
 
 private:
     vector<double> array;
@@ -56,45 +47,33 @@ private:
     void stringToArray(string in_str) {
       istringstream stream(in_str);
       int numElts;
-
       stream >> numElts;
 
       vector<double> createArray(numElts);
-
       for (int i = 0; i < numElts; i++) {
         stream >> createArray[i];
       }
 
+      sort(createArray.begin(),createArray.end());
       array = createArray;
     }
-
 };
 
 
 int main(void)
 {
-
   cout << "Please enter, on one line, the count of numbers followed by the numbers themselves, separated by spaces.\nEnter a blank line to exit.\n\n";
 
   string str;
 
   while ( getline( cin , str ) ) {
-
     if ( str.length( ) == 0 )
         break;
 
     BasicStats stats(str);
-    double min = stats.min();
-    double max = stats.max();
-    double median = stats.median();
-    double mean = stats.mean();
 
-    cout << "Thanks!\n" << "The minimum is " << min << ",\nthe maximum is " << max << ",\nthe median is " << median<< ",\nand the mean is " << mean << ".\n\n";
-
+    cout << "Thanks!\n" << "The minimum is " << stats.min() << ",\nthe maximum is " << stats.max() << ",\nthe median is " << stats.median()<< ",\nand the mean is " << stats.mean() << ".\n\n";
   }
-
-
-//  return;
 
   return 0;
 }
