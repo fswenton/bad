7eeee01cb6d8f9ad8f2fd5d820c8482ce4aae437
sam-dewolf <62413716+sam-dewolf@users.noreply.github.com>
2021-02-28 15:56:24 -0500

    Class inital assignment
    Refreshed a given program to make it easier to read and cleaner.

--- main.cpp
+++ main.cpp
@@ -5,122 +5,95 @@
 
 using namespace std;
 
-// This function computes the mean of a vector of doubles called "a"
-double m0(vector<double> a)
-{
-  // This is the sum
-  double s = 0.0;
+class Numbers{
+public:
+    Numbers(string s){
+        this->s = s;
+    }
 
- // Loop through all elements of the array
-  for (unsigned int i = 0; i < a.size(); ++i)
-  {
-    // Add the element to the sum
-    s += a[i];
-  }
+    // This where the magic happens!
+    void getInfo() {
+      // Turn our string of input into an array
+      vector<double> a = string2array(s);
 
-   // The mean is the sum divided by the count---return that
-  return s/a.size();
+      // Be polite and give the the user the basic stats on their data
+      cout << "Thanks!\n" << "The minimum is " << minVal(a) << ",\nthe maximum is " << maxVal(a) << ",\nthe median is " << medianVal(a) << ",\nand the mean is " << meanVal(a) << ".\n\n";
 
-}
+      return;
+    }
+private:
+    string s;
 
-// This function computes the minimum value of a vector of doubles called "a"
-double m1(vector<double> a)
-{
+    // This function computes the mean of a vector of doubles called "a"
+    double meanVal(vector<double> a) {
+      // This is the sum
+      double sum = 0.0;
 
-  // First, we sort the array
-  sort(a.begin(), a.end());
+      // Loop through all elements of the array
+      for (unsigned int i = 0; i < a.size(); ++i)
+      {
+        // Add the element to the sum
+        sum += a[i];
+      }
 
-  // Now we just return the first element, which after sorting is the minimum
-  return a[0];
+      // The mean is the sum divided by the count---return that
+      return sum/a.size();
+    }
 
-}
+    // This function computes the minimum value of a vector of doubles called "a"
+    double minVal(vector<double> a) {
+      return *min_element(a.begin(), a.end());
+    }
 
-// This function computes the maximum value of a vector of doubles called "a"
-double m2(vector<double> a)
-{
+    // This function computes the maximum value of a vector of doubles called "a"
+    double maxVal(vector<double> a) {
+      return *max_element(a.begin(), a.end());
+    }
 
-  // First, we sort the array
-  sort(a.begin(), a.end());
+    // This function computes the median of a vector of doubles called "a"
+    double medianVal(vector<double> a) {
+      // First, we sort the array
+      sort(a.begin(), a.end());
 
-  // Now we just return the last element, which after sorting is the Maximum
-  return a[ a.size() - 1 ];
+      // This bit's a little tricky
+      if (a.size() % 2) {
+        // If we have an odd number of elements, the median is the middle one
+        return a[(a.size() - 1 ) / 2];
+      } else {
+        // Otherwise, we average the values of the two middlemost elements
+        return 0.5*(a[a.size() / 2] + a[a.size() / 2 - 1]);
+      }
+    }
 
-}
+    // This will parse a line of whitespace-separated numbers into an array
+    // The first number is the count of numbers, and the rest are the numbers
+    vector<double> string2array(string s) {
+      istringstream iss(s);
 
-// This function computes the median of a vector of doubles called "a"
-double m3(vector<double> a)
-{
+      // "n" is the number of elements in our array, which comes first
+      int n;
 
-  // First, we sort the array
-  sort(a.begin(), a.end());
+      iss >> n;
 
-  // This bit's a little tricky
-  if (a.size() % 2)
-  {
-    // If we have an odd number of elements, the median is the middle one
-    return a[ (a.size() - 1 ) / 2];
-  }
-  else
-  {
-    // Otherwise, we average the values of the two middlemost elements
-    return 0.5*(a[ a.size() / 2 ] + a[ a.size() / 2 - 1]);
-  }
+      vector<double> a(n);
 
-}
+      for (int i = 0; i < n; ++i) {
+        iss >> a[i];
+      }
+      return a;
+    }
+};
 
-// This will parse a line of whitespace-separated numbers into an array
-// The first number is the count of numbers, and the rest are the numbers
-vector<double> s2a(string s) {
-  istringstream iss(s);
-
-  // "n" is the number of elements in our array, which comes first
-  int n;
-
-  iss >> n;
-
-  vector<double> a(n);
-
-  for (int i = 0;
-       i < n;
-       ++i)
-  {
-    iss >> a[i];
-  }
-
-
-  return a;
-
-}
-
-// This where the magic happens!
-void go(string s)
-{
-
-  // Turn our string of input into an array (above)
-  vector<double> a = s2a(s);
-
-  // Be polite and give the the user the basic stats on their data
-  cout << "Thanks!\n" << "The minimum is " << m1(a) << ",\nthe maximum is " << m2(a) << ",\nthe median is " << m3(a) << ",\nand the mean is " << m0(a) << ".\n\n";
-
-  return;
-
-}
-
-int main(void)
-{
-
+int main(void) {
   cout << "Please enter, on one line, the count of numbers followed by the numbers themselves, separated by spaces.\nEnter a blank line to exit.\n\n";
 
   string s;
 
-  while ( getline( cin , s ) ) {
-
-    if ( s.length( ) == 0 )
-        break;
-
-    go(s);
-
+  while (getline(cin , s)) {
+    if (s.length() == 0)
+      break;
+    Numbers nums(s);
+    nums.getInfo();
   }
-
   return 0;
 }
