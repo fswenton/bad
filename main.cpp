496922d18dd4b18ac621f4d49c6d774d0087bf7b
Sam Marshall <79521697+gsmarshall@users.noreply.github.com>
2021-02-28 11:22:14 -0500

    Recreating changes to main.cpp in order to restructure local files

--- main.cpp
+++ main.cpp
@@ -5,122 +5,85 @@
 
 using namespace std;
 
-// This function computes the mean of a vector of doubles called "a"
-double m0(vector<double> a)
-{
-  // This is the sum
-  double s = 0.0;
+/*
+ * Changes:
+ * - better function names
+ * - deleted unnecessary comments and whitespace
+ * - restructured as class with statistics as members, which are calculated in methods
+ */
 
- // Loop through all elements of the array
-  for (unsigned int i = 0; i < a.size(); ++i)
-  {
-    // Add the element to the sum
-    s += a[i];
-  }
+// calculates basic statistics for a vector of doubles
+class Stats {
+public:
+    vector<double> data;
+    double mean;
+    double min;
+    double max;
+    double median;
 
-   // The mean is the sum divided by the count---return that
-  return s/a.size();
+    Stats (vector<double> _data): data(_data){
+        sort(data.begin(), data.end());
+        mean = 0.0;
+        min = 0.0;
+        max = 0.0;
+        median = 0.0;
+    };
 
-}
+    friend double calcMean(Stats input) {
+        double sum = 0.0;
+        for (unsigned int i = 0; i < input.data.size(); ++i) { sum += input.data[i]; }
+        return sum/input.data.size();
+    }
 
-// This function computes the minimum value of a vector of doubles called "a"
-double m1(vector<double> a)
-{
+    friend double calcMin(Stats sorted) { return sorted.data[0]; }
 
-  // First, we sort the array
-  sort(a.begin(), a.end());
+    friend double calcMax(Stats sorted) { return sorted.data[ sorted.data.size() - 1 ]; }
 
-  // Now we just return the first element, which after sorting is the minimum
-  return a[0];
+    friend double calcMedian(Stats sorted) {
+        if (sorted.data.size() % 2) {
+          // If we have an odd number of elements, the median is the middle one
+          return sorted.data[ (sorted.data.size() - 1 ) / 2];
+        }
+        else {
+          // Otherwise, we average the values of the two middlemost elements
+          return 0.5*(sorted.data[ sorted.data.size() / 2 ] + sorted.data[ sorted.data.size() / 2 - 1 ]);
+        }
+      }
+};
 
-}
-
-// This function computes the maximum value of a vector of doubles called "a"
-double m2(vector<double> a)
-{
-
-  // First, we sort the array
-  sort(a.begin(), a.end());
-
-  // Now we just return the last element, which after sorting is the Maximum
-  return a[ a.size() - 1 ];
-
-}
-
-// This function computes the median of a vector of doubles called "a"
-double m3(vector<double> a)
-{
-
-  // First, we sort the array
-  sort(a.begin(), a.end());
-
-  // This bit's a little tricky
-  if (a.size() % 2)
-  {
-    // If we have an odd number of elements, the median is the middle one
-    return a[ (a.size() - 1 ) / 2];
-  }
-  else
-  {
-    // Otherwise, we average the values of the two middlemost elements
-    return 0.5*(a[ a.size() / 2 ] + a[ a.size() / 2 - 1]);
-  }
-
+Stats calcStats(vector<double> input) {
+    Stats raw (input);
+    raw.mean = calcMean(raw);
+    raw.min = calcMin(raw);
+    raw.max = calcMax(raw);
+    raw.median = calcMedian(raw);
+    return raw;
 }
 
 // This will parse a line of whitespace-separated numbers into an array
-// The first number is the count of numbers, and the rest are the numbers
-vector<double> s2a(string s) {
-  istringstream iss(s);
+vector<double> parseString(string input) {
+  istringstream iss(input);
 
-  // "n" is the number of elements in our array, which comes first
-  int n;
+  int num_elements;
+  iss >> num_elements;
 
-  iss >> n;
-
-  vector<double> a(n);
-
-  for (int i = 0;
-       i < n;
-       ++i)
-  {
-    iss >> a[i];
-  }
-
-
-  return a;
-
+  vector<double> rawData(num_elements);
+  for (int i = 0; i < num_elements; ++i) { iss >> rawData[i]; }
+  return rawData;
 }
 
-// This where the magic happens!
-void go(string s)
-{
-
-  // Turn our string of input into an array (above)
-  vector<double> a = s2a(s);
-
-  // Be polite and give the the user the basic stats on their data
-  cout << "Thanks!\n" << "The minimum is " << m1(a) << ",\nthe maximum is " << m2(a) << ",\nthe median is " << m3(a) << ",\nand the mean is " << m0(a) << ".\n\n";
-
+void go(string rawData) {
+  Stats processed = calcStats(parseString(rawData));
+  cout << "Thanks!\n" << "The minimum is " << processed.min << ",\nthe maximum is " << processed.max << ",\nthe median is " << processed.median << ",\nand the mean is " << processed.mean << ".\n\n";
   return;
-
 }
 
-int main(void)
-{
-
+int main(void) {
   cout << "Please enter, on one line, the count of numbers followed by the numbers themselves, separated by spaces.\nEnter a blank line to exit.\n\n";
-
   string s;
-
   while ( getline( cin , s ) ) {
-
-    if ( s.length( ) == 0 )
-        break;
-
+    if ( s.length( ) == 0 ) { break; }
     go(s);
-
   }
-
   return 0;
 }
