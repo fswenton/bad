d9a61dc8644e87677f8752164beecf7db7f8a666
Isabelle Cochran <isabellecochran@Isabelles-MacBook-Pro.local>
2021-02-28 17:00:38 -0500

    Cleaned up code. Removed comments, changed variable and method names and removed uncessary white space

--- main.cpp
+++ main.cpp
@@ -1,126 +1,60 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-// This function computes the mean of a vector of doubles called "a"
-double m0(vector<double> a)
-{
-  // This is the sum
-  double s = 0.0;
-
- // Loop through all elements of the array
-  for (unsigned int i = 0; i < a.size(); ++i)
-  {
-    // Add the element to the sum
-    s += a[i];
+double mean(vector<double> a) {
+  double sum = 0.0;
+  for (unsigned int i = 0; i < a.size(); ++i) {
+    sum += a[i];
   }
-
-   // The mean is the sum divided by the count---return that
-  return s/a.size();
-
+  return sum/a.size();
 }
 
-// This function computes the minimum value of a vector of doubles called "a"
-double m1(vector<double> a)
-{
-
-  // First, we sort the array
-  sort(a.begin(), a.end());
-
-  // Now we just return the first element, which after sorting is the minimum
+double min(vector<double> a) {
   return a[0];
-
 }
 
-// This function computes the maximum value of a vector of doubles called "a"
-double m2(vector<double> a)
-{
-
-  // First, we sort the array
-  sort(a.begin(), a.end());
-
-  // Now we just return the last element, which after sorting is the Maximum
-  return a[ a.size() - 1 ];
-
+double max(vector<double> a) {
+  return a[a.size() - 1];
 }
 
-// This function computes the median of a vector of doubles called "a"
-double m3(vector<double> a)
-{
-
-  // First, we sort the array
-  sort(a.begin(), a.end());
-
-  // This bit's a little tricky
-  if (a.size() % 2)
-  {
-    // If we have an odd number of elements, the median is the middle one
-    return a[ (a.size() - 1 ) / 2];
-  }
+double median(vector<double> a) {
+  // If we have an odd number of elements, the median is the middle one
+  if (a.size() % 2 != 0)
+    return a[(a.size()-1) / 2];
+  // Otherwise, we average the values of the two middlemost elements
   else
-  {
-    // Otherwise, we average the values of the two middlemost elements
-    return 0.5*(a[ a.size() / 2 ] + a[ a.size() / 2 - 1]);
-  }
-
+    return 0.5*(a[a.size() / 2] + a[a.size() / 2 - 1]);
 }
 
 // This will parse a line of whitespace-separated numbers into an array
 // The first number is the count of numbers, and the rest are the numbers
-vector<double> s2a(string s) {
+vector<double> makeArray(string s) {
   istringstream iss(s);
+  int count;
+  iss >> count;
+  vector<double> a(count);
 
-  // "n" is the number of elements in our array, which comes first
-  int n;
-
-  iss >> n;
-
-  vector<double> a(n);
-
-  for (int i = 0;
-       i < n;
-       ++i)
-  {
+  for (int i = 0; i < count; ++i) {
     iss >> a[i];
   }
-
-
   return a;
-
 }
 
-// This where the magic happens!
-void go(string s)
-{
-
-  // Turn our string of input into an array (above)
-  vector<double> a = s2a(s);
-
-  // Be polite and give the the user the basic stats on their data
-  cout << "Thanks!\n" << "The minimum is " << m1(a) << ",\nthe maximum is " << m2(a) << ",\nthe median is " << m3(a) << ",\nand the mean is " << m0(a) << ".\n\n";
-
-  return;
-
-}
-
-int main(void)
-{
-
-  cout << "Please enter, on one line, the count of numbers followed by the numbers themselves, separated by spaces.\nEnter a blank line to exit.\n\n";
-
+int main(void) {
+  cout << "Please enter, on one line, the count of numbers followed by the numbers themselves, "
+  "separated by spaces.\nEnter a blank line to exit.\n\n";
   string s;
 
-  while ( getline( cin , s ) ) {
-
-    if ( s.length( ) == 0 )
+  while (getline(cin, s)) {
+    if (s.length() == 0)
         break;
-
-    go(s);
-
+    vector<double> a = makeArray(s);
+    sort(a.begin(), a.end());
+    cout << "Thanks!\n" << "The minimum is " << min(a) << ",\nthe maximum is " << max(a) <<
+            ",\nthe median is " << median(a) << ",\nand the mean is " << mean(a) << ".\n\n";
   }
-
   return 0;
 }
