478c482b32964650c5b825617c0cf5b87c84cd3f
Jackson Chen <jacksonc@middlebury.edu>
2021-03-01 20:11:19 -0500

    Changed variable and method names to be more clear. Structured the methods code into a class.

--- main.cpp
+++ main.cpp
@@ -5,83 +5,83 @@
 
 using namespace std;
 
-double findMean(vector<double> vectorOfNums) {
-  double sum = 0.0;
+/* A simple statstic program that calculates
+ * mean, max, minimum, and median of a list of numbers */
 
-  for(unsigned int i = 0; i < vectorOfNums.size(); ++i) {
-    sum += vectorOfNums[i];
-  }
+class CalcStat {
+public:
+    vector<double> numList;
 
-  return sum/vectorOfNums.size();
-}
+    // Construtor
+    CalcStat(string inputStr) {
+        stringToArray(inputStr);
+    }
 
-double findMin(vector<double> vectorOfNums) {
-  sort(vectorOfNums.begin(), vectorOfNums.end());
+    // Methods
+    double mean() {
+        double sum = 0.0;
 
-  return vectorOfNums[0];
-}
+        for(unsigned int i = 0; i < numList.size(); ++i) {
+          sum += numList[i];
+        }
 
-double findMax(vector<double> vectorOfNums) {
-  sort(vectorOfNums.begin(), vectorOfNums.end());
+        return sum/numList.size();
+    }
 
-  return vectorOfNums[ vectorOfNums.size() - 1 ];
-}
+    double min() {
+      return numList[0];
+    }
 
-double findMedian(vector<double> vectorOfNums) {
-  sort(vectorOfNums.begin(), vectorOfNums.end());
+    double max() {
+      return numList[numList.size() - 1];
+    }
 
-  if (vectorOfNums.size() % 2) {
-    // If we have an odd number of elements, the median is the middle one
-    return vectorOfNums[ (vectorOfNums.size() - 1 ) / 2];
-  }
-  else {
-    // Otherwise, we average the values of the two middlemost elements
-    return 0.5*(vectorOfNums[ vectorOfNums.size() / 2 ] + vectorOfNums[ vectorOfNums.size() / 2 - 1]);
-  }
-}
+    double median() {
+      if (numList.size() % 2) {
+        // If we have an odd number of elements, the median is the middle one
+        return numList[(numList.size() - 1 ) / 2];
+      }
+      else {
+        // Otherwise, we average the values of the two middlemost elements
+        return 0.5*(numList[numList.size() / 2] + numList[numList.size() / 2 - 1]);
+      }
+    }
 
-// This will parse a line of whitespace-separated numbers into an array
-// The first number is the count of numbers, and the rest are the numbers
-vector<double> stringToArray(string inputString) {
-  istringstream iss(inputString);
+private:
+    // This will parse a line of whitespace-separated numbers into an array
+    // The first number is the count of numbers, and the rest are the numbers
+    void stringToArray(string inputStr) {
+      istringstream strStream(inputStr);
+      int numElements;
 
-  // "n" is the number of elements in our array, which comes first
-  int n;
+      strStream >> numElements;
+      vector<double> inputList(numElements);
 
-  iss >> n;
+      for(int i = 0; i < numElements; ++i) {
+        strStream >> inputList[i];
+      }
 
-  vector<double> VectorOfNums(n);
+      sort(inputList.begin(), inputList.end());
 
-  for(int i = 0; i < n; ++i) {
-    iss >> VectorOfNums[i];
-  }
-
-  return VectorOfNums;
-}
-
-void calculateStatistic(string inputString) {
-
-  vector<double> vectorOfNums = stringToArray(inputString);
-
-  cout << "Thanks!\n" << "The minimum is " << findMin(vectorOfNums)
-       << ",\nthe maximum is " << findMax(vectorOfNums)
-       << ",\nthe median is " << findMedian(vectorOfNums)
-       << ",\nand the mean is " << findMean(vectorOfNums) << ".\n\n";
-
-  return;
-}
+      numList = inputList;
+    }
+};
 
 int main() {
-  string inputString;
+  string inputStr;
 
   cout << "Please enter, on one line, the count of numbers followed by the numbers themselves, separated by spaces.\nEnter a blank line to exit.\n\n";
 
-  while(getline(cin, inputString)) {
-
-    if(inputString.length() == 0)
+  while(getline(cin, inputStr)) {
+    if(inputStr.length() == 0)
         break;
 
-    calculateStatistic(inputString);
+    CalcStat getStat(inputStr);
+
+    cout << "Thanks!\n" << "The minimum is " << getStat.min()
+         << ",\nthe maximum is " << getStat.max()
+         << ",\nthe median is " << getStat.median()
+         << ",\nand the mean is " << getStat.mean() << ".\n\n";
   }
 
   return 0;
