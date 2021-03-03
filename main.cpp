1db70dcd7b554be427a1d15e6507a11b8a94a288
Farhan Islam <fislam@middlebury.edu>
2021-03-02 22:14:58 -0500

    Used OOP

--- main.cpp
+++ main.cpp
@@ -5,76 +5,86 @@
 
 using namespace std;
 
-// This function computes the mean of a vector of doubles called "a"
-double mean(vector<double> a){
-  double sum = 0.0;
-  for (unsigned int i = 0; i < a.size(); ++i){
-    sum += a[i];
-  }
-  return sum/a.size();
-}
+class Statistics{
+    bool isSorted = false;
+    vector<double> array;
+public: 
+    // This will parse a line of whitespace-separated numbers into an array
+    // The first number is the count of numbers, and the rest are the numbers
+    Statistics(string s) {
+        istringstream iss(s);
+        int n; //Number of elements is first element
+        iss >> n;
+        array.resize(n);
+        for (int i = 0;i < n;++i){
+            iss >> array[i];
+        }
+    }
+    
+    void sortIfNeeded(){
+        if (isSorted) return;
+        sort(array.begin(), array.end());
+        isSorted = true;
+        return;
+    }
 
-// This function computes the minimum value of a vector of doubles called "a"
-double min(vector<double> a){
-  sort(a.begin(), a.end());
-  return a[0];
-}
+    // This function computes the mean of a vector of doubles called "a"
+    double mean(){
+      double sum = 0;
+      for (unsigned int i = 0; i < array.size(); ++i){
+        sum += array[i];
+      }
+      return sum/array.size();
+    }
 
-// This function computes the maximum value of a vector of doubles called "a"
-double max(vector<double> a){
-  sort(a.begin(), a.end());
-  return a[ a.size() - 1 ];
-}
+    // This function computes the minimum value of a vector of doubles called "a"
+    double min(){
+      sortIfNeeded();
+      return array[0];
+    }
 
-// This function computes the median of a vector of doubles called "a"
-double median(vector<double> a){
-  sort(a.begin(), a.end());
-  if (a.size() % 2){
-    // If we have an odd number of elements, the median is the middle one
-    return a[(a.size()-1)/2];
-  }else{
-    // Otherwise, we average the values of the two middlemost elements
-    return 0.5*(a[ a.size() / 2 ] + a[ a.size() / 2 - 1]);
-  }
-}
+    // This function computes the maximum value of a vector of doubles called "a"
+    double max(){
+      sortIfNeeded();
+      return array[array.size() - 1];
+    }
 
-// This will parse a line of whitespace-separated numbers into an array
-// The first number is the count of numbers, and the rest are the numbers
-vector<double> stringToArray(string s) {
-  istringstream iss(s);
+    // This function computes the median of a vector of doubles called "a"
+    double median(){
+      sortIfNeeded();
+      if (array.size() % 2){
+        // If we have an odd number of elements, the median is the middle one
+        return array[(array.size() - 1)/2];
+      }else{
+        // Otherwise, we average the values of the two middlemost elements
+        return 0.5*(array[array.size()/2] + array[array.size()/2 - 1]);
+      }
+    }
 
-  // "n" is the number of elements in our array, which comes first
-  int n;
-  iss >> n;
-  vector<double> a(n);
-  for (int i = 0;i < n;++i){
-    iss >> a[i];
-  }
-  return a;
+    // This where the magic happens!
+    void printStats(){
+      cout << "Thanks!\n" 
+           << "The minimum is " << min() 
+           << ",\nthe maximum is " << max() 
+           << ",\nthe median is " << median() 
+           << ",\nand the mean is " << mean() << ".\n\n";
+      return;
+    }
 
-}
+};
 
-// This where the magic happens!
-void go(string s){
-  // Turn our string of input into an array (above)
-  vector<double> a = stringToArray(s);
-  // Be polite and give the the user the basic stats on their data
-  cout << "Thanks!\n" << "The minimum is " << min(a) << ",\nthe maximum is " << max(a) << ",\nthe median is " << median(a) << ",\nand the mean is " << mean(a) << ".\n\n";
-
-  return;
-}
 
 int main(void){
-
   cout << "Please enter, on one line, the count of numbers followed by the numbers themselves, separated by spaces.\nEnter a blank line to exit.\n\n";
   string s;
-  while ( getline( cin , s ) ) {
-    if ( s.length( ) == 0)
+  while (getline(cin,s)){
+    if (s.length() == 0)
         break;
-    if (s == "0"){
+    if (s == "0")
         cout << "Nothing to show!\n";
-    }else{
-        go(s);
+    else{ 
+        Statistics stats(s);
+        stats.printStats();
     }
   }
   return 0;
